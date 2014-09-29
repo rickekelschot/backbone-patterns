a71994b9e61f70cbb2d44ffb9b74df6bb7c925f7
Rick Ekelschot <rick@frontmen.nl>
2014-09-29 16:42:39 +0200

    Added different render types, callback & return

--- src/js/view/render.js
+++ src/js/view/render.js
@@ -1,18 +1,26 @@
 Backbone.View.prototype.renderMethod = "append"; //append, replace, prepend
+Backbone.View.prototype.renderType = "callback"; //callback or return
 
 Backbone.View.prototype.render = (function () {
     if (typeof this.template !== 'function') {
         throw Error('Template is not a function!');
     }
+    var appendView = (function (element) {
+            if (this.renderMethod === 'replace') {
+                this.$el = $(element);
+            } else {
+                this.$el[this.renderMethod](
+                    element
+                );
+            }
+        }.bind(this));
 
-    if (this.renderMethod === 'replace') {
-        this.$el = $(
-            this.template(this.getTemplateData())
-        );
+    if (this.renderType === 'return') {
+        appendView(this.template(this.getTemplateData()));
     } else {
-        this.$el[this.renderMethod](
-            this.template(this.getTemplateData())
-        );
+         this.template(this.getTemplateData(), function (out) {
+             appendView(out);
+         });
     }
 });
 
