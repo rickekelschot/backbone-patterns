2a4790913b8fd2bb7b4846795c84d9137c4e6800
Rick Ekelschot <rick@frontmen.nl>
2014-09-30 11:57:03 +0200

    Fixed bug

--- dist/backbone-patterns.js
+++ dist/backbone-patterns.js
@@ -192,6 +192,25 @@ Backbone.View.prototype.render = (function () {
 Backbone.View.prototype.getTemplateData = (function () {
     return null;
 });
+;Backbone.View.prototype._subscribeToEvents = (function () {
+    if (typeof this.subscriptions !== 'undefined') {
+        for (var key in this.subscriptions) {
+            if (typeof this[this.subscriptions[key]] === 'function') {
+                this.subscribe(key, this[this.subscriptions[key]], this);
+            }
+        }
+    }
+});
+
+Backbone.View.prototype._unSubscribeToEvents = (function () {
+    if (typeof this.subscriptions !== 'undefined') {
+        for (var key in this.subscriptions) {
+            if (typeof this[this.subscriptions[key]] === 'function') {
+                this.unsubscribe(key, this[this.subscriptions[key]], this);
+            }
+        }
+    }
+});
 ;var oldRemove = Backbone.View.prototype.remove;
 Backbone.View.prototype.remove = (function () {
     this._unSubscribeToEvents();
