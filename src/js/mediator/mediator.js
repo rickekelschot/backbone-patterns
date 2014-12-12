7c038fa1f83919e1e8b6b58f100a38f57e64a7b0
Rick Ekelschot <rick@frontmen.nl>
2014-12-12 14:30:05 +0100

    Added request name to catch-unregistered

--- src/js/mediator/mediator.js
+++ src/js/mediator/mediator.js
@@ -36,6 +36,11 @@ _.extend(Backbone.mediator.prototype, {
             var scope = handler.scope || null,
                 props = (arguments.length > 1) ? _.toArray(arguments).slice(1) : [];
 
+            //Add request name to start of arguments
+            if (handler === this.handlers['catch-unregistered']) {
+                props.unshift(name);
+            }
+
             return handler.responder.apply(scope, props);
         }
         throw Error('Backbone.mediator -> Response handler for (' + name + ') is not registered and there is no catch-unregistered handler registered');
