b026ededc1b0ef24c71fcec43ebc959313e1092f
Rick Ekelschot <rick@frontmen.nl>
2015-06-29 14:43:04 +0200

    Added Backbone.history & Router pre-route event

--- dist/backbone-patterns.js
+++ dist/backbone-patterns.js
@@ -575,8 +575,12 @@
     });
     Backbone.Router.prototype.execute = function(callback, args) {
         Backbone.history.trigger('pre-route', args);
+        this.trigger('pre-route', args);
+    
         if (callback) callback.apply(this, args);
+    
         Backbone.history.trigger('post-route', args);
+        this.trigger('post-route', args);
     };
     var oldProto = Backbone.View.prototype,
         extend = Backbone.View.extend,
--- src/js/router/execute.js
+++ src/js/router/execute.js
@@ -1,5 +1,9 @@
 Backbone.Router.prototype.execute = function(callback, args) {
     Backbone.history.trigger('pre-route', args);
+    this.trigger('pre-route', args);
+
     if (callback) callback.apply(this, args);
+
     Backbone.history.trigger('post-route', args);
+    this.trigger('post-route', args);
 };
